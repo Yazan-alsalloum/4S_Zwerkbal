32224cf64a0f2ac51a85d069863ecec0d73ff566
Yazan-alsalloum <121229781+Yazan-alsalloum@users.noreply.github.com>
2023-09-12 13:14:54 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,22 @@
 
 @section('content')
 
-    <h1>Test</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, eligendi porro! Asperiores odio deleniti voluptatibus obcaecati corporis tempora quos numquam molestiae! Dolorum quis hic doloribus debitis nesciunt minima, quisquam optio.</p>
+<h1>Dashboard</h1>
+<p>Zwerkbal (Engels: Quidditch) is een sport voor tovenaars uit de Harry Potterboeken van J.K. Rowling. In de wereld van Harry Potter is Zwerkbal een populaire sport: veel mensen praten erover mee en zijn zeer enthousiast als er een groot toernooi wordt gespeeld.
+     Ook heeft iedere heks of tovenaar wel een favoriet Zwerkbalteam.</p>
+<div class="datum-img" >
+    <div>
+        <div class="aantall-tornooien">
+            <h1>Aantal aankomende toernooien: </h1>
+            <h1>5</h1>
+        </div>
+        <div class="aantall-teams">
+            <h1>Totaal aantal teams:</h1>
+            <h1>26</h1>
+        </div>
+    </div>
 
+
+    <img class="img" src="/img/snitch.png">
+</div>
 @endsection
