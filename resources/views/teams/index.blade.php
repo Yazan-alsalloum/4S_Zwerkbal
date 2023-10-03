05a741ecff1558598cc5f624f150415fa90b7570
Yazan-alsalloum <121229781+Yazan-alsalloum@users.noreply.github.com>
2023-10-03 11:38:19 +0200

    creat index for teams

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -1,2 +1,39 @@
+<!-- Hier komt een lijst van teams -->
+@extends('layout')
 
-<!-- hier komt een lijst van teams -->
+@section('nav')
+    <nav>
+        <a href="/">Home</a>
+        <a href="/tournaments">Toernooien</a>
+        <a href="/teams" class="active">Teams</a> <!-- De active-klasse is toegevoegd aan Teams -->
+        <a href="/players">Spelers</a>
+    </nav>
+@endsection
+
+@section('content')
+    @if($errors->any())
+        <ul class="errors">
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+    <h1>Teams</h1>
+    <a href="{{ route('teams.create') }}">+ nieuw team</a>
+    <table class="table">
+        <tr>
+            <th>Team</th>
+            <th>Soort</th>
+            <th>Herkomst</th>
+        </tr>
+        @foreach($teams as $team)
+            <tr>
+                <td>{{ $team->name }}</td>
+
+                <td>{{ $team->type }}</td>
+
+                <td>{{ $team->origin }}</td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
