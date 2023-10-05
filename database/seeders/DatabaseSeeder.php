318a4fc63435a660c51a7e1c172321b9e7f5f204
Yazan-alsalloum <121229781+Yazan-alsalloum@users.noreply.github.com>
2023-10-05 16:05:56 +0200

    Seeder bijgewerkt met nieuwe team- en spelergegevens

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,14 +17,37 @@ class DatabaseSeeder extends Seeder
         //
     	// Je kunt hier je eigen seeders invoegen:
         //
+//team
 
+    $team = new \App\Models\Team();
+    $team->name = "German National Quidditch team";
+    $team->type = "country";
+    $team->origin = "Duitsland";
+    $team->save();
+
+    $team = new \App\Models\Team();
+    $team->name = "Hufflepuff";
+    $team->type = "school";
+    $team->origin = "Zweinstein";
+    $team->save();
+    $team = new \App\Models\Team();
+    $team->name = "4S Quidditch Team";
+    $team->type = "commercial";
+    $team->save();
+
+
+     // SPELERS
+
+    $characters = ["Ludo Bagman", "Argus Filch", "Marvolo Gaunt", "Rubeus Hagrid", "Lee Jordan", "Viktor Krum", "Draco Malfoy", "Padma Patil", "Newt Scamander", "Fred Weasley", "Oliver Wood", "Horace Slughorn", "Alicia Spinnet", "Garrick Ollivander", "Luna Lovegood", "Neville Longbottom", "Gregory Goyle", "Colin Creevey", "Susan Bones", "Hannah Abbott"];
+    $types = ["chaser", "beater", "keeper", "seeker"];
+    $teams = \App\Models\Team::all();
 
 
 
         //Tournament 1
         $tournament = new Tournament();
         $tournament->name = "EK Zwerkbal " . date('Y');
-        $tournament->date = date("Y-m-d", strtotime("+14 days"));
+        $tournament->date = date("Y-m-d", strtotime("14 days"));
         $tournament->start_time = rand(1, 20) . ":00:00";
         $tournament->save();
 
@@ -37,8 +60,8 @@ class DatabaseSeeder extends Seeder
 
         //Tournament 3
         $tournament = new Tournament();
-        $tournament->name = "Scholentoernooi " . (date('Y')+1);
-        $tournament->date = date("Y-m-d", strtotime("+13 months"));
+        $tournament->name = "Scholentoernooi " . (date('Y'));
+        $tournament->date = date("Y-m-d", strtotime("13 months"));
         $tournament->save();
 
         //Tournament 4
@@ -47,12 +70,26 @@ class DatabaseSeeder extends Seeder
         $tournament->date = date("Y-m-d");
         $tournament->start_time = rand(1, 20) . ":00:00";
         $tournament->save();
+                // SPELERS
+                //
+                $characters = ["Ludo Bagman", "Argus Filch", "Marvolo Gaunt", "Rubeus Hagrid", "Lee Jordan", "Viktor Krum", "Draco Malfoy", "Padma Patil", "Newt Scamander", "Fred Weasley", "Oliver Wood", "Horace Slughorn", "Alicia Spinnet", "Garrick Ollivander", "Luna Lovegood", "Neville Longbottom", "Gregory Goyle", "Colin Creevey", "Susan Bones", "Hannah Abbott"];
+                $types = ["chaser", "beater", "keeper", "seeker"];
+                $teams = \App\Models\Team::all();
 
+                foreach($characters as $character)
+                {
+                    $player = new \App\Models\Player();
+                    $player->team_id = $teams->random()->id;
+                    $player->name = $character;
+                    $player->type = $types[array_rand($types)];
+                    $player->save();
+                }
         //
         // NIETS AANPASSEN TUSSEN DEZE REGELS!
-        // ~!@#$%^&**()_+
-        // +_)(*&^%$#@!~
+        // ~!@#$%^&**()_
+        // _)(*&^%$#@!~
         // NIETS AANPASSEN TUSSEN DEZE REGELS!
         //
     }
 }
+
