438a7b5661e1ecb3b9085d5e21493d5beaa981e1
Yazan-alsalloum <121229781+Yazan-alsalloum@users.noreply.github.com>
2023-10-05 16:12:49 +0200

    team index  bijgewerkt met spelersinformatie

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -7,6 +7,7 @@
         <a href="/tournaments">Toernooien</a>
         <a href="/teams" class="active">Teams</a> <!-- De active-klasse is toegevoegd aan Teams -->
         <a href="/players">Spelers</a>
+        <a href="/upcoming">upcoming</a>
     </nav>
 @endsection
 
@@ -25,15 +26,28 @@
             <th>Team</th>
             <th>Soort</th>
             <th>Herkomst</th>
+            <th>Spelers</th>
         </tr>
         @foreach($teams as $team)
             <tr>
                 <td>{{ $team->name }}</td>
-
                 <td>{{ $team->type }}</td>
-
                 <td>{{ $team->origin }}</td>
+                <td>
+                    <ul>
+                        @foreach($team->players->groupBy('type') as $type => $players)
+                            <li><strong>{{ $type }}:</strong></li>
+                            <ul>
+                                @foreach($players->sortBy('name') as $player)
+                                    <li>{{ $player->name }} ({{ $player->type }})</li>
+                                @endforeach
+                            </ul>
+                        @endforeach
+                    </ul>
+
+                </td>
             </tr>
+            
         @endforeach
     </table>
 @endsection
